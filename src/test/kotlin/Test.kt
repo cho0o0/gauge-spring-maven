47ff5fca4f7183e171b3e3e2a39ae7abd49e4a9d
CHO <cho0o0@users.noreply.github.com>
2018-08-30 18:57:03 +0900

    fix test case

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,37 +1,63 @@
-import com.gargoylesoftware.htmlunit.Page
-import com.thoughtworks.gauge.Step
-import io.github.cho0o0.gaugespringmaven.ProfileController
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
-import org.springframework.web.context.WebApplicationContext
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder
-import com.thoughtworks.gauge.Table
-
+import com.thoughtworks.gauge.*
+import io.github.bonigarcia.wdm.ChromeDriverManager
+import junit.framework.TestCase.assertFalse
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.chrome.ChromeDriver
+import org.springframework.boot.web.server.LocalServerPort
 
 class Test {
-    @Autowired
-    private lateinit var context: WebApplicationContext
+//    @Autowired
+//    private lateinit var context: WebApplicationContext
+
+    private lateinit var driver: WebDriver
+
+    @LocalServerPort
+    private val port: Int = 8080
+
+    @BeforeSpec
+    @BeforeClass
+    @Throws(Exception::class)
+    fun setUpClass() {
+        ChromeDriverManager.getInstance().setup()
+    }
+
+    @Before
+    @BeforeScenario
+    @Throws(Exception::class)
+    fun setUp() {
+        driver = ChromeDriver()
+    }
+
+    @After
+    @AfterScenario
+    fun tearDown() {
+        driver.close()
+    }
+
+//    @Step("プロフィール画面にいます")
+//    fun testProfilePageAlter() {
+//        val action = get("/profile")
+//        val mockMvc: MockMvc = MockMvcBuilders.standaloneSetup(ProfileController())
+//                .apply<StandaloneMockMvcBuilder>(sharedHttpSession())
+//                .build()
+//        mockMvc.perform(action).andExpect(status().is2xxSuccessful)
+//    }
 
     @Step("プロフィール画面にいます")
     fun testProfilePage() {
-        val action = get("/profile")
-        val mockMvc: MockMvc = MockMvcBuilders.standaloneSetup(ProfileController())
-                .apply<StandaloneMockMvcBuilder>(sharedHttpSession())
-                .build()
-        mockMvc.perform(action).andExpect(status().is2xxSuccessful)
+        driver.navigate().to("http://localhost:$port/profile?name=test")
     }
 
     @Step("歓迎メッセージが表示される <table>")
     fun testWelcomeMessage(table: Table) {
-//        val webClient = MockMvcWebClientBuilder
-//                .webAppContextSetup(context)
-//                .build()
-//        val page: Page = webClient.getPage<Page>("http://localhost:8080/profile?name=test")
-//        assert(page.isHtmlPage)
+        val body: WebElement = driver.findElement(By.tagName("body"))
+        val bodyText: String = body.text
+        val possibleMessages: List<String> = table.getColumnValues("歓迎メッセージ")
+        assertFalse(possibleMessages.filter { bodyText.contains(it) }.isEmpty())
     }
 }
